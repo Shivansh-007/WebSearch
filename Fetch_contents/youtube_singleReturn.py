9093a3c09d911b49a84b865f3d5e30818cfbc325
NMarco <53913990+marco-create@users.noreply.github.com>
2020-09-01 08:41:47 +0200

    Update youtube_singleReturn.py
    try except if user inputs wrong apimkey

--- Fetch_contents/youtube_singleReturn.py
+++ Fetch_contents/youtube_singleReturn.py
@@ -26,6 +26,20 @@ def yt_searchtopic(topic, key=api_key, build_yt=youtube):
 		vids_urls.append(['https://www.youtube.com/embed/' + vid_url,vid_title])
 	return vids_urls
 
+
+test_api = True
+
 if __name__ == '__main__':
-	topic = input('>> Insert topic: ')
-	print(yt_searchtopic(topic))
+	while test_api == True: 
+		try:
+			print('PASTE YOUR YOUTUBE API KEY HERE (no quotes needed): ')
+			api_key = input()
+			youtube = build('youtube', 'v3', developerKey= api_key)
+
+			topic = input('>> Insert topic: ')
+			print(yt_searchtopic(topic, api_key, build_yt=youtube))
+
+			test_api = False
+
+		except:
+			print("Youtube API Key Error. The API key hasn't been entered in yet, isn't correctly formatted as a string, or is invalidated.\n")
